983f1746cf07a514ed9ab524a3ef56f25811c5bb
José Yépez <jl.yg91@gmail.com>
2026-03-06 08:46:21 -0500

    Refactor code structure for improved readability and maintainability

--- config/settings.py
+++ config/settings.py
@@ -179,7 +179,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
